da44fd926509cb7f3b4cef71e01cb84ace086582
Jada Hundley <jadahundley@gmail.com>
2023-07-16 20:14:24 -0500

    updated shuttle requirements

--- scriptHelper.js
+++ scriptHelper.js
@@ -17,11 +17,48 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
-   
+    if (testInput === ""){
+        return "Empty";  
+    } else if (isNaN(testInput)) {
+        return "Is a Number";
+    } else {
+        return "Not a Number";
+    }
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+    let pilotStatus = document.getElementById('pilotStatus');
+    let copilotStatus = document.getElementById('copilotStatus');
+    let fuelStatus = document.getElementById('fuelStatus');
+    let launchStatus = document.getElementById('launchStatus');
+    let cargoStatus = document.getElementById('cargoStatus');
+
+    if (validateInput(pilot) === "Empty" || validateInput(copilot) === "Empty" || 
+    validateInput(fuelLevel) === "Empty" || validateInput(cargoLevel) === "Empty") {
+        alert("Answer is required");
+    } else if (validateInput(fuelLevel) === "Not a Number" || validateInput(cargoLevel) === "Not a Number") {
+        alert("Enter numerical value");
+    } else if (validateInput(pilot) === "Is a Number" || validateInput(copilot) === "Is a Number") {
+        alert("Enter name");
+    } else {
+        pilotStatus.innerHTML = `Pilot ${pilot} is ready`;
+        copilotStatus.innerHTML = `Co-pilot ${copilot} is ready`;
+    }
+
+    if (fuelLevel < 10000) {
+        fuelStatus.innerHTML = "Not enough fuel for the journey";
+        list.styles.visibility = "visible";
+        launchStatus.innerHTML = "Shuttle not ready for launch";
+        launchStatus.styles.color = "red";
+    } else if (cargoLevel > 10000) {
+        cargoStatus.innerHTML = "Too much mass for the shuttle to take off";
+        list.styles.visibility = "visible";
+        launchStatus.innerHTML = "Shuttle not ready for launch";
+        launchStatus.styles.color = "red";
+    } else {
+        launchStatus.innerHTML = "Shuttle is ready for launch";
+        launchStatus.styles.color = "green";
+    }
 }
 
 async function myFetch() {
